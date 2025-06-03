f08bdaa984fc95a523586272816712eb1fb22e17
sechignffc <gmsitcs@@gmail.com>
2025-06-03 13:59:48 +0100

    Change BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./style.css";
 import reportWebVitals from "./reportWebVitals";
 
-const repoName = "/midessexboxingreact";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router basename={repoName}>
+  <Router>
     <App />
   </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
